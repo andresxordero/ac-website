ecdf7cba7a94fe6164a095422a36a4c7e1812fa8
Andrés Cordero <andresmcorderor@gmail.com>
2024-12-17 06:30:35 -0600

    fix: gradient an grabbing on projects section

--- src/components/common/cards/ProjectsSectionCard.tsx
+++ src/components/common/cards/ProjectsSectionCard.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable tailwindcss/migration-from-tailwind-2 */
+import { useState } from 'react';
 import { FaExternalLinkSquareAlt } from 'react-icons/fa';
 
 import { DynamicButton } from '../buttons/DynamicButton';
@@ -13,17 +14,27 @@ export default function ProjectsSectionCard({
   link,
   title,
 }: ProjectsSectionCardImage) {
+  const [isGrabbing, setIsGrabbing] = useState(false);
+
   const handleOpenLink = () => {
     window.open(link, '_blank');
   };
 
   return (
     <div
-      className="bg-cover bg-center p-24 "
-      style={{ backgroundImage: `url(${image})`, height: '100vh' }}
+      className={`relative bg-cover bg-center p-24 ${
+        isGrabbing ? 'cursor-grabbing' : 'cursor-grab'
+      }`}
+      style={{
+        backgroundImage: `linear-gradient(to top, var(--black), transparent), url(${image})`,
+        height: '100vh',
+      }}
+      onMouseDown={() => setIsGrabbing(true)}
+      onMouseUp={() => setIsGrabbing(false)}
+      onMouseLeave={() => setIsGrabbing(false)}
     >
-      <div className="absolute inset-x-0 bottom-0 bg-gradient-to-t from-[var(--black)] to-transparent px-24 py-12">
-        <h3 className="font-dm-sans  w-3/4 text-[3rem] font-bold leading-tight text-[var(--white)]">
+      <div className="absolute inset-x-0 bottom-0 px-24 py-12">
+        <h3 className="font-dm-sans w-3/4 text-[3rem] font-bold leading-tight text-[var(--white)]">
           {title}
         </h3>
 
